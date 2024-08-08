b5aca1095ddaafbfef88f1ccb7ff13cbe518a820
Uli3000 <ulisesgamer281@gmail.com>
2024-08-07 20:07:56 -0600

    DetalleFinal

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack } from '@mui/material'
+import { Stack } from '@mui/material'
 import './App.css'
 import MyAppBar from './components/MyAppBar'
 import TutorCard from './components/TutorCard'
